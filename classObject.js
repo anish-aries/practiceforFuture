20b10fcde80a02930b0b2be176cbb26791f12b1e
Anish Adhikari <73144665+anish-aries@users.noreply.github.com>
2024-02-10 21:53:19 +0545

    First js commit

---
+++ classObject.js
@@ -0,0 +1,12 @@
+class Hello
+ {
+    constructor(name, address, phone)
+    {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+}
+
+const a = new Hello("Ram", "btm", 9812345670);
+console.log(a);
