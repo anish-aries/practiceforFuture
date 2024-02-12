78968eb572571bf6beea9f1bf9979d04545fe5d1
Anish Adhikari <73144665+anish-aries@users.noreply.github.com>
2024-02-12 13:27:19 +0545

    now commit

--- jsPractice/mainModule.js
+++ jsPractice/mainModule.js
@@ -1,4 +1,9 @@
 import students from "./referenceModule";
 
+
+let greet = 'hello';
+let name = 'anish';
+let age = 22;
+
 console.log(students);
 // you are able to access data of other module by exporting and importing.........
